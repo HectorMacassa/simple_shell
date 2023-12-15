adb208012a6af3041088efad15f7f5b06ae11ad8
Hector Macassa <cbmacassa@gmail.com>
2023-12-15 10:38:49 -0800

    Simple Shell

--- alias_history_operations.c
+++ alias_history_operations.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * display_history - displays the history list, one command by line, preceded
+ * _myhistory - displays the history list, one command by line, preceded
  *                   with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * Return: Always 0
  */
-int display_history(info_t *info)
+int _myhistory(info_t *info)
 {
 	print_list(info->history);
 	return (0);
@@ -78,12 +78,12 @@ int print_alias(list_t *node)
 }
 
 /**
- * handle_alias - handles the alias builtin (man alias)
+ * _myalias - handles the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-int handle_alias(info_t *info)
+int _myalias(info_t *info)
 {
 	int i = 0;
 	char *p = NULL;
--- shell_exec.c
+++ shell_exec.c
@@ -66,13 +66,13 @@ int find_builtin(info_t *info)
 	int i, built_in_ret = -1;
 	builtin_table builtintbl[] = {
 		{"exit", _myexit},
-	/*	{"env", _myenv},*/
+		{"env", _myenv},
 		{"help", _myhelp},
-	/*	{"history", _myhistory},*/
-	/*	{"setenv", _mysetenv},*/
-	/*	{"unsetenv", _myunsetenv},*/
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
 		{"cd", _mycd},
-	/*	{"alias", _myalias},*/
+		{"alias", _myalias},
 		{NULL, NULL}
 	};
 
